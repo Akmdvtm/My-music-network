b836e360d2296fa490938be2be7a8ecb001473c8
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-18 17:12:11 +0600

    updated some files and added homeview

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <HeroSection></HeroSection>
+  </div>
+</template>
+
+<script setup>
+import HeroSection from "@/components/partials/HeroSection.vue";
+</script>
+
+<style lang="scss" scoped>
+
+</style>
