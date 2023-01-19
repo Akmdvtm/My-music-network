ecb7805542697f45b70f39a80ea4e097e0957661
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-19 23:16:25 +0600

    add navBar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <TopNavigation></TopNavigation>
     <HeroSection></HeroSection>
   </div>
 </template>
 
 <script setup>
 import HeroSection from "@/components/partials/HeroSection.vue";
+import TopNavigation from "@/components/stucture/TopNavigation.vue";
 </script>
 
 <style lang="scss" scoped>
