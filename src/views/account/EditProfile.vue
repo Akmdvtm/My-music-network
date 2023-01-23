583bbe425c12d142d8177457f68ac43e0351bc1d
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-23 17:37:17 +0600

    update edit profile, add modal component for cropper image

--- src/views/account/EditProfile.vue
+++ src/views/account/EditProfile.vue
@@ -2,6 +2,13 @@
   <div id="EditProfile" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
     <div class="text-gray-900 text-xl">Edit Profile</div>
     <div class="bg-green-500 w-full h-1"></div>
+    <CropperModal
+        v-if="showModal"
+        :minAspectRatioProp="{width: 8, height:8}"
+        :maxAspectRatioProp="{width: 8, height:8}"
+        @croppedImageData="setCroppedImageData"
+        @showModal="showModal = false"
+    ></CropperModal>
     <div class="flex flex-wrap mt-4 mb-6">
       <div class="w-full md:w-1/2 px-3">
         <TextInput
@@ -39,7 +46,17 @@
         <DisplayCropperButton
             label="Profile Image"
             btnText="Update Profile Image"
+            @showModal="showModal = true"
         ></DisplayCropperButton>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap mt-4 mb-6">
+      <div class="w-full md:w-1/2 px-3">
+        <CroppedImage
+            label="Cropped Image"
+            :image="image"
+        ></CroppedImage>
       </div>
     </div>
 
@@ -72,8 +89,12 @@ import {ref} from "vue";
 import DisplayCropperButton from "@/components/global/DisplayCropperButton.vue";
 import TextArea from "@/components/global/TextArea.vue";
 import SubmitFormButton from "@/components/global/SubmitFormButton.vue";
+import CropperModal from "@/components/global/CropperModal.vue";
+import CroppedImage from "@/components/global/CroppedImage.vue";
 
+let image = ref(null)
 let firstName = ref(null)
+let showModal = ref(false)
 let lastName = ref(null)
 let location = ref(null)
 let description = ref(null)
